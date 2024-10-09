1dc09d8bff718106bfd2e0d04f60ea32a478b33e
n-csc <nich5@student.eadania.dk>
2024-10-09 14:03:18 +0200

    test

--- dj.py
+++ dj.py
@@ -1,48 +1,61 @@
 import RPi.GPIO as GPIO
 import time
 
-# Indstil GPIO pins for sensor inputs
+# Pin-konfiguration
 left_sensor = 17  # Venstre sensor til GPIO17
 right_sensor = 27  # Højre sensor til GPIO27
 
-# Indstil GPIO til motor styring
-motor_left_forward = 22
-motor_left_backward = 23
-motor_right_forward = 24
-motor_right_backward = 25
+# Motor driver pins
+IN1 = 22  # Motor 1 retning
+IN2 = 23  # Motor 1 retning
+IN3 = 24  # Motor 2 retning
+IN4 = 25  # Motor 2 retning
+ENA = 12  # PWM til motor 1 hastighed
+ENB = 13  # PWM til motor 2 hastighed
 
 # Opsæt GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(left_sensor, GPIO.IN)
 GPIO.setup(right_sensor, GPIO.IN)
-GPIO.setup(motor_left_forward, GPIO.OUT)
-GPIO.setup(motor_left_backward, GPIO.OUT)
-GPIO.setup(motor_right_forward, GPIO.OUT)
-GPIO.setup(motor_right_backward, GPIO.OUT)
+
+GPIO.setup(IN1, GPIO.OUT)
+GPIO.setup(IN2, GPIO.OUT)
+GPIO.setup(IN3, GPIO.OUT)
+GPIO.setup(IN4, GPIO.OUT)
+GPIO.setup(ENA, GPIO.OUT)
+GPIO.setup(ENB, GPIO.OUT)
+
+# Opret PWM objekter
+pwm_left = GPIO.PWM(ENA, 100)  # 100Hz PWM signal til venstre motor
+pwm_right = GPIO.PWM(ENB, 100)  # 100Hz PWM signal til højre motor
+
+# Start PWM ved 50% duty cycle
+pwm_left.start(50)
+pwm_right.start(50)
 
 def move_forward():
-    GPIO.output(motor_left_forward, True)
-    GPIO.output(motor_left_backward, False)
-    GPIO.output(motor_right_forward, True)
-    GPIO.output(motor_right_backward, False)
+    GPIO.output(IN1, True)
+    GPIO.output(IN2, False)
+    GPIO.output(IN3, True)
+    GPIO.output(IN4, False)
 
 def turn_left():
-    GPIO.output(motor_left_forward, False)
-    GPIO.output(motor_left_backward, True)
-    GPIO.output(motor_right_forward, True)
-    GPIO.output(motor_right_backward, False)
+    GPIO.output(IN1, False)
+    GPIO.output(IN2, True)
+    GPIO.output(IN3, True)
+    GPIO.output(IN4, False)
 
 def turn_right():
-    GPIO.output(motor_left_forward, True)
-    GPIO.output(motor_left_backward, False)
-    GPIO.output(motor_right_forward, False)
-    GPIO.output(motor_right_backward, True)
+    GPIO.output(IN1, True)
+    GPIO.output(IN2, False)
+    GPIO.output(IN3, False)
+    GPIO.output(IN4, True)
 
 def stop():
-    GPIO.output(motor_left_forward, False)
-    GPIO.output(motor_left_backward, False)
-    GPIO.output(motor_right_forward, False)
-    GPIO.output(motor_right_backward, False)
+    GPIO.output(IN1, False)
+    GPIO.output(IN2, False)
+    GPIO.output(IN3, False)
+    GPIO.output(IN4, False)
 
 try:
     while True:
@@ -60,4 +73,6 @@ try:
         time.sleep(0.1)
 
 except KeyboardInterrupt:
+    pwm_left.stop()
+    pwm_right.stop()
     GPIO.cleanup()

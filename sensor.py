09e02065bfa437ca186348e4e7cc99da3749e3c1
djmhd <djmhd2604@gmail.com>
2024-10-10 14:16:33 +0200

    jnw

--- sensor.py
+++ sensor.py
@@ -9,8 +9,8 @@ right_sensor = 23  # GPIO pin til højre sensor
 DIR1_Front = 22  # Retningskontrol forreste venstre motor
 PWM1_Front = 6   # Hastighedskontrol forreste venstre motor
 
-DIR2_Front = 0   # Retningskontrol forreste højre motor
-PWM2_Front = 12  # Hastighedskontrol forreste højre motor
+DIR2_Front = 0    # Retningskontrol forreste højre motor
+PWM2_Front = 12   # Hastighedskontrol forreste højre motor
 
 # Definer GPIO pins for TB6612 H-broer (bageste hjul)
 DIR1_Back = 3    # Retningskontrol bageste venstre motor
@@ -19,11 +19,10 @@ PWM1_Back = 24   # Hastighedskontrol bageste venstre motor
 DIR2_Back = 14   # Retningskontrol bageste højre motor
 PWM2_Back = 10    # Hastighedskontrol bageste højre motor
 
-# Opsæt GPIO pins som input for sensorerne og output for motorerne
+# Opsæt GPIO pins
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(left_sensor, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Pulldown-modstand
-GPIO.setup(right_sensor, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Pulldown-modstand
 
+# Opsæt motorpins som output
 GPIO.setup(DIR1_Front, GPIO.OUT)
 GPIO.setup(PWM1_Front, GPIO.OUT)
 GPIO.setup(DIR2_Front, GPIO.OUT)
@@ -34,11 +33,15 @@ GPIO.setup(PWM1_Back, GPIO.OUT)
 GPIO.setup(DIR2_Back, GPIO.OUT)
 GPIO.setup(PWM2_Back, GPIO.OUT)
 
-# Opret PWM objekter til hastighedskontrol (100Hz frekvens)
-pwm_front_left = GPIO.PWM(PWM1_Front, 100)
-pwm_front_right = GPIO.PWM(PWM2_Front, 100)
-pwm_back_left = GPIO.PWM(PWM1_Back, 100)
-pwm_back_right = GPIO.PWM(PWM2_Back, 100)
+# Opsæt sensorpins som input med pulldown-modstand
+GPIO.setup(left_sensor, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Pulldown-modstand
+GPIO.setup(right_sensor, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  # Pulldown-modstand
+
+# Opret PWM objekter til hastighedskontrol
+pwm_front_left = GPIO.PWM(PWM1_Front, 100)  # 100Hz til venstre forreste motor
+pwm_front_right = GPIO.PWM(PWM2_Front, 100)  # 100Hz til højre forreste motor
+pwm_back_left = GPIO.PWM(PWM1_Back, 100)  # 100Hz til venstre bageste motor
+pwm_back_right = GPIO.PWM(PWM2_Back, 100)  # 100Hz til højre bageste motor
 
 # Start PWM med 0% duty cycle
 pwm_front_left.start(0)
@@ -46,20 +49,42 @@ pwm_front_right.start(0)
 pwm_back_left.start(0)
 pwm_back_right.start(0)
 
-# Funktioner til motorstyring
-def set_individual_speeds(front_left_speed, front_right_speed, back_left_speed, back_right_speed):
-    # Juster hastigheder individuelt for hver motor og reducer med 10%
-    pwm_front_left.ChangeDutyCycle(front_left_speed * 0.9)  # 90% af max hastighed
-    pwm_front_right.ChangeDutyCycle(front_right_speed * 0.9)  # 90% af max hastighed
-    pwm_back_left.ChangeDutyCycle(back_left_speed * 0.9)  # 90% af max hastighed
-    pwm_back_right.ChangeDutyCycle(back_right_speed * 0.9)  # 90% af max hastighed
+# Funktioner til at styre motorerne
+def move_forward():
+    # Sæt retningen til fremad
+    GPIO.output(DIR1_Front, GPIO.HIGH)
+    GPIO.output(DIR2_Front, GPIO.HIGH)
+    GPIO.output(DIR1_Back, GPIO.LOW)
+    GPIO.output(DIR2_Back, GPIO.LOW)
+    pwm_back_left.ChangeDutyCycle(100)
+    pwm_back_right.ChangeDutyCycle(100)
+    pwm_front_left.ChangeDutyCycle(100)
+    pwm_front_right.ChangeDutyCycle(100)
+
+def move_backward():
+    # Sæt retningen til baglæns
+    GPIO.output(DIR1_Front, GPIO.LOW)
+    GPIO.output(DIR2_Front, GPIO.LOW)
+    GPIO.output(DIR1_Back, GPIO.HIGH)
+    GPIO.output(DIR2_Back, GPIO.HIGH)
+    pwm_back_left.ChangeDutyCycle(100)
+    pwm_back_right.ChangeDutyCycle(100)
+    pwm_front_left.ChangeDutyCycle(100)
+    pwm_front_right.ChangeDutyCycle(100)
 
 def stop_motors():
-    # Stop alle motorer
+    # Stop motorerne
     pwm_front_left.ChangeDutyCycle(0)
     pwm_front_right.ChangeDutyCycle(0)
     pwm_back_left.ChangeDutyCycle(0)
     pwm_back_right.ChangeDutyCycle(0)
+
+def set_speed(left_speed, right_speed):
+    # Juster hastighed for venstre og højre side
+    pwm_front_left.ChangeDutyCycle(left_speed)
+    pwm_front_right.ChangeDutyCycle(right_speed)
+    pwm_back_left.ChangeDutyCycle(left_speed)
+    pwm_back_right.ChangeDutyCycle(right_speed)
 
 # Hovedprogram til at styre motorer baseret på sensorer
 try:
@@ -73,22 +98,29 @@ try:
 
         # Motorstyring baseret på sensor-input
         if left_value == 0 and right_value == 0:
-            # Begge sensorer ser hvidt -> Kør fremad med reduceret hastighed
-            set_individual_speeds(70, 70, 70, 70)  # Sænk hastigheden på alle hjul
+            # Begge sensorer ser hvidt -> Kør fremad
+            move_forward()
         elif left_value == 0 and right_value == 1:
-            # Venstre sensor ser hvid -> Stop venstre forhjul og baghjul, højre kører hurtigere
-            set_individual_speeds(0, 70, 0, 70)  # Stop venstre hjul, højre kører fuld kraft
+            # Venstre sensor ser hvid -> Stop venstre hjul og drej til højre
+            pwm_back_left.ChangeDutyCycle(0)
+            pwm_front_left.ChangeDutyCycle(0)
+            pwm_back_right.ChangeDutyCycle(100)
+            pwm_front_right.ChangeDutyCycle(100)
         elif left_value == 1 and right_value == 0:
-            # Højre sensor ser hvid -> Stop højre forhjul og baghjul, venstre kører hurtigere
-            set_individual_speeds(70, 0, 70, 0)  # Stop højre hjul, venstre kører fuld kraft
+            # Højre sensor ser hvid -> Stop højre hjul og drej til venstre
+            pwm_back_right.ChangeDutyCycle(0)
+            pwm_front_right.ChangeDutyCycle(0)
+            pwm_back_left.ChangeDutyCycle(100)
+            pwm_front_left.ChangeDutyCycle(100)
         else:
-            # Begge sensorer ser sort -> Kør fremad med reduceret hastighed
-            set_individual_speeds(60, 60, 60, 60)  # Fuld hastighed på alle hjul
+            # Begge sensorer ser sort -> Stop motorerne
+            stop_motors()
 
         time.sleep(0.1)  # Vent lidt før næste aflæsning
 
 except KeyboardInterrupt:
     # Stop motorer og ryd op når programmet afbrydes
+    stop_motors()
     pwm_front_left.stop()
     pwm_front_right.stop()
     pwm_back_left.stop()
